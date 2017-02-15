af9c763adf6badfa0367f18ff29f89d4443e18c1
Eliot Carlsen and Max Larson <max@>
2017-02-14 16:49:52 -0800

    add js functions and images to cat/dog fighting page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
 $ (document).ready(function() {
   $("#catPic").click(function() {
-    $("#catPic").hide("#bark");
+    $("#bark").hide();
     $("#meow").toggle();
   });
   $("#dogPic").click(function() {
+    $("#meow").hide();
     $("#bark").toggle();
+  });
+  $("#saveCat").click(function() {
+    $("#meow").remove();
+    $("#bark").remove();
+    $("#ruffery").toggle();
+  });
+  $("#feed").click(function() {
+    $("li#feed").append('<img src="img/food.jpeg" />');
+  });
+  $("#pet").click(function() {
+    $("li#pet").append('<img src="img/dogpetting.jpeg" />');
+  });
+  $("#walk").click(function() {
+    $("li#walk").append('<img src="img/catwalk.jpeg" />');
+  });
+  $("#kick").click(function() {
+    $("li#kick").append('<img src="img/kickdog.jpeg" />');
   });
 });
